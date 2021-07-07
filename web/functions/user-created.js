d35180b45e35f3b02befe05133b9a71d93812dd0
Riley Cook <rileycookd@gmail.com>
2021-07-07 16:23:57 -0400

    test adding sanity student from netlify function

--- web/functions/user-created.js
+++ web/functions/user-created.js
@@ -6,46 +6,40 @@ const client = sanityClient({
   useCDN: false,
 })
 
-exports.handler = async function (event, context, callback) {
-  // Pulling out the payload from the body
-  const { payload } = JSON.parse(event.body)
-  // Checking which form has been submitted
-  const isRegistrationForm = payload.data.formId === "registration-form"
-  const isContactForm = payload.data.formId === "contact-form"
-  // Build the document JSON and submit it to SANITY
-  if (isRegistrationForm) {
-    weeklySchedule=[];
-    if(payload.data.classSchedule1) weeklySchedule.push(payload.data.classSchedule1)
-    if(payload.data.classSchedule2) weeklySchedule.push(payload.data.classSchedule2)
-    if(payload.data.classSchedule3) weeklySchedule.push(payload.data.classSchedule3)
-    const registrationForm = {
-      _type: "registrationForm",
-      submitDate: new Date().toISOString(),
-      name: payload.data.name,
-      email: payload.data.email,
-      location: payload.data.location,
-      classType: payload.data.classType,
-      level: payload.data.level,
-      classSize: payload.data.classSize,
-      duration: payload.data.duration,
-      quantity: payload.data.quantity,
-      frequency: payload.data.frequency,
-      timezone: payload.data.timezone,
-      schedule: weeklySchedule
+exports.handler = (event, context, callback) => {
+  const { email, password, name } = JSON.parse(event.body);
+    
+  const sanityUser = {
+      _type: "student",
+      name: name,
+      email: email
     }
-    const result = await client.create(registrationForm).catch((err) => console.log(err))
-  }
-  if (isContactForm) {
-    const contactForm = {
-      _type: "contactForm",
-      submitDate: new Date().toISOString(),
-      name: payload.data.name,
-      email: payload.data.email,
-      message: payload.data.message
-    }
-    const result = await client.create(contactForm).catch((err) => console.log(err))
+    const result = await client.create(sanityUser).catch((err) => console.log(err))
   }
   callback(null, {
     statusCode: 200,
   })
+
+  // adminAuth
+  //   .createUser({email, password})
+  //   .then(userRecord => {
+  //     callback(null, {
+  //       statusCode: 200
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     let msg = '';
+
+  //     if (error.code === 'auth/invalid-password') {
+  //       msg = `L'identifiant de l'appareil doit avoir au moins 6 caractères`;
+  //     }
+  //     else if (error.code === 'auth/email-already-exists') {
+  //       msg = `L'email indiqué est déjà utilisé`;
+  //     }
+
+  //     callback(null, {
+  //       statusCode: 400,
+  //       body: JSON.stringify({code: error.code, msg})
+  //     });
+  //   });
 }
